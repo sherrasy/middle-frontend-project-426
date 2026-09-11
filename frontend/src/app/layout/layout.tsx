dad0b30623bb82282172297acc7d54d750a4566c
Anastasia <dzheyn.anna@mail.ru>
2026-09-11 21:00:23 +0400

    add sentry

--- frontend/src/app/layout/layout.tsx
+++ frontend/src/app/layout/layout.tsx
@@ -1,8 +1,12 @@
 import { Outlet } from 'react-router-dom';
+import { ErrorButton } from './errorButton';
 
 export const MainLayout = () => (
   <div className='min-h-screen bg-gray-50 text-gray-900'>
     {/* <Header /> */}
+    <div className='absolute top-4 right-4'>
+      <ErrorButton />
+    </div>
     <main
       className={`max-w-6xl mx-auto px-4 py-8 flex flex-col items-center gap-6`}
     >
--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,6 +3,9 @@ import { createRoot } from 'react-dom/client';
 import { RouterProvider } from 'react-router-dom';
 import { router } from './app/router/router';
 import './app/styles/index.css';
+import { initSentry } from './shared/config/initSentry';
+
+initSentry();
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
