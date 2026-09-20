cdb90252e4f0d07d42f30c99a5bb428e07cf9471
Anastasia <dzheyn.anna@mail.ru>
2026-09-20 19:32:41 +0400

    change axios base

--- frontend/src/shared/config/axiosApi.ts
+++ frontend/src/shared/config/axiosApi.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 import { LOCALSTORAGE_NAMES } from '../constants/localstorage-names';
 
 export const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || 'http://localhost:3000/api',
+  baseURL: '/api',
   headers: {
     'Content-Type': 'application/json',
   },
